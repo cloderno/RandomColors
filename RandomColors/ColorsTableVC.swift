1371782960cbbf1d2af346c32b02ed61f39709d0
Rating <achubenko@rating.kz>
2023-11-28 11:49:59 +0600

    created enums to safety purposes from static strings

--- RandomColors/ColorsTableVC.swift
+++ RandomColors/ColorsTableVC.swift
@@ -10,6 +10,14 @@ import UIKit
 class ColorsTableVC: UIViewController {
     var colors: [UIColor] = []
     
+    enum Cells {
+        static let colorCell = "ColorCell"
+    }
+    
+    enum Segues {
+        static let toDetail = "ToColorDetailVC"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,7 +47,7 @@ extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // for rows that are populated, to prevent loading all of the rows
         // use guard to check if it's not empty to not get null value
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell") else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Cells.colorCell) else {
             return UITableViewCell()
         }
         cell.backgroundColor = colors[indexPath.row]
@@ -49,6 +57,6 @@ extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let color = colors[indexPath.row]
-        performSegue(withIdentifier: "ToColorDetailVC", sender: color)
+        performSegue(withIdentifier: Segues.toDetail, sender: color)
     }
 }
